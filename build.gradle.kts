3c59f7f7c48a591b239f2db8c64cc01b119f2084
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-09-11 05:05:46 +0200

    bump intellij platform gradle property to 2.18.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.4.0"
-    id("org.jetbrains.intellij.platform") version "2.10.4"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
 }
 
 group = providers.gradleProperty("pluginGroup").get()
