124ac98dcadc260bd870e06e6bca3c0b7bf78d68
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-09-11 05:49:26 +0200

    Feature/design basic plugin (#3)
    * First pass of setting plugin infrastructure
    
    * address gradle compile issues
    
    * bump intellij platform gradle property to 2.18.1
    
    * do not track .intellijPlatform
    
    * leverage toml file for common version properties
    
    * Add icons
    
    * Add Changelog
    
    * Correct colour of main icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,103 @@
 plugins {
-    kotlin("jvm") version "2.4.0"
+    id("java")
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.intellijPlatform)
+    alias(libs.plugins.changelog)
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
+
+// Set the JVM language level used to build the project.
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        freeCompilerArgs.add("-Xjvm-default=all")
+    }
+}
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+changelog {
+    groups.empty()
+    repositoryUrl = "https://github.com/alexBlakeGoudemond/jetbrains-window-accent"
 }
 
 dependencies {
+    intellijPlatform {
+        intellijIdea(providers.gradleProperty("platformVersion"))
+        jetbrainsRuntime()
+    }
     testImplementation(kotlin("test"))
 }
 
-kotlin {
-    jvmToolchain(21)
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild.set(providers.gradleProperty("pluginSinceBuild"))
+        }
+
+        val changelogHandle = changelog
+        changeNotes.set(
+            providers.gradleProperty("pluginVersion").map {
+                changelogHandle.renderItem(
+                    changelogHandle.get(it).withHeader(false).withEmptySections(false),
+                    org.jetbrains.changelog.Changelog.OutputType.HTML,
+                )
+            }
+        )
+    }
+
+    instrumentCode = false
+    buildSearchableOptions = false
+
+    // to run manually: `./gradlew verifyPlugin --rerun-tasks --info`
+    pluginVerification {
+        // Suppress known platform threading bugs that appear in the verifier report but have no
+        // plugin code in their stack trace. See plugin-verifier-ignored-problems.txt for details.
+        ignoredProblemsFile = layout.projectDirectory.file("plugin-verifier-ignored-problems.txt")
+
+        ides {
+            // See https://www.jetbrains.com/idea/download/other/ for other versions
+            create("IU", "2026.1.2")
+        }
+    }
+
+    publishing {
+        token = providers.gradleProperty("intellijPlatformPublishingToken")
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks{
+    test {
+        useJUnitPlatform()
+    }
+    instrumentCode {
+        enabled = false
+    }
+    instrumentTestCode {
+        enabled = false
+    }
+    wrapper {
+        gradleVersion = providers.gradleProperty("gradleVersion").get()
+    }
+
+    // not building gradle plugin, so dont load it
+    runIde {
+        systemProperty("idea.max.content.load.filesize", "2000000")
+        // Enable heap snapshot on plugin unload failure for classloader leak diagnosis.
+        // When ide.plugins.snapshot.on.unload.fail=true, a .hprof is written to the user
+        // home directory if a plugin fails the classloader GC check during dynamic unload.
+        jvmArgs("-XX:+UnlockDiagnosticVMOptions")
+        systemProperty("ide.plugins.snapshot.on.unload.fail", "true")
+        // Enable internal mode so ClassLoaderLeakDiagnostics (and other internal tooling
+        // such as LeakHunter) is active during runIde sessions.
+        systemProperty("idea.is.internal", "true")
+    }
 }
--- src/main/kotlin/com/kudos/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.kudos
-
-fun main() {
-    print("Hello, World!")
-}
